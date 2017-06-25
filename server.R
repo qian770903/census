8bd8d7a6f130ff3c1b7cfd0dde4d953f82e8d7ac
qian770903 <4755882@qq.com>
2017-06-25 09:05:13 +0800

    Update server.R

--- server.R
+++ server.R
@@ -1,36 +1,3 @@
 # server.R
-
-library(maps)
-library(mapproj)
-counties <- readRDS("data/counties.rds")
-source("helpers.R")
-
-shinyServer(
-  function(input, output) {
-    output$map <- renderPlot({
-      data <- switch(input$var, 
-                     "Percent White" = counties$white,
-                     "Percent Black" = counties$black,
-                     "Percent Hispanic" = counties$hispanic,
-                     "Percent Asian" = counties$asian)
-      
-      color <- switch(input$var, 
-                      "Percent White" = "darkgreen",
-                      "Percent Black" = "black",
-                      "Percent Hispanic" = "darkorange",
-                      "Percent Asian" = "darkviolet")
-      
-      legend <- switch(input$var, 
-                       "Percent White" = "% White",
-                       "Percent Black" = "% Black",
-                       "Percent Hispanic" = "% Hispanic",
-                       "Percent Asian" = "% Asian")
-      
-      percent_map(var = data, 
-                  color = color, 
-                  legend.title = legend, 
-                  max = input$range[2], 
-                  min = input$range[1])
-    })
-  }
-)
+library(quantmod)source("helpers.R")
+shinyServer(function(input, output) {    dataInput <- reactive({      getSymbols(input$symb, src = "yahoo",                from = input$dates[1],               to = input$dates[2],               auto.assign = FALSE)  })    finalInput <- reactive({    if (!input$adjust) return(dataInput())    adjust(dataInput())  })    output$plot <- renderPlot({    chartSeries(finalInput(), theme = chartTheme("white"),                 type = "line", log.scale = input$log, TA = NULL)  })})
